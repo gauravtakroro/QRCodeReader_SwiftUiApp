bb410e947b9f18e96418ad72d40bb187a71d2887
Gaurav Tak <gaurav.tak@roro.io>
2023-08-24 22:56:53 +0530

    Initial Commit

---
+++ QRCodeReader-SwiftUi/QRCodeReader_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeReader_SwiftUiApp.swift
+//  QRCodeReader-SwiftUi
+//
+//  Created by Roro Solutions LLP on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeReader_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
